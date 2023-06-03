3fe4967121c83919523dfddfd36e4a88018d1f59
NoraGamero <noriyuky82@gmail.com>
2023-06-03 11:20:42 -0600

    Mis practicas de python

---
+++ variables.py/operadores.py
@@ -0,0 +1,38 @@
+# Operadores aritmeticos
+# print(2 + 2) # suma
+# print(4 - 2) # resta
+# print(3 * 7) # multiplicacion
+# print(15 / 2 )# division
+# print(11 % 4) # modulo
+# print(11 // 4) # division de piso
+# print(2 ** 3) # potenciacion
+
+# # Operadores en cadenas
+# print('hola' + 'mundo') # concatenacion
+# print('hola' * 3) # repeticion
+# print('hola' + 'mundo' * 3)
+
+# # Operadores de comparacion
+# print('Hola' == 'hola') # igual que
+# print('Hola' != 'hola') # distinto que
+# print(3 > 11) # mayor que
+# print(11 >= 10) # mayor o igual
+# print(10 <= 10) # menor o igual
+
+# Operadores de existencia
+# print('ola' in 'Hola') # existencia
+# print('ola' not in 'Hola') # inexistencia
+
+# Operadores Booleanos
+# print(True or False) # or verifica que se cumpla solo una condicion
+# print(True and False) # and verifica que se cumplan ambas condiciones
+
+# Operadores de Asignacion - da un nvo valor a uno que ya tenia
+# x = 1 # operador de asignacion estandar
+# x += 2 # operador de asignacion suma
+# x *= 3 # operador de asignacion multiplicacion
+# x %= 4 # operador de asignacion módulo
+# print(x)
+
+x = 6 * 5 + 8 / 2 ** 4
+print (x)
